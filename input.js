db55d10086efb664d1239e32a49f3b9705391d70
akaz9579 <akaz9579@gmail.com>
2025-09-07 23:57:30 -0400

    test input

---
+++ input.js
@@ -0,0 +1,12 @@
+//Dynamic types for vars
+
+let username;
+
+username = window.prompt("What is your username?");
+console.log(username);
+
+let us
+document.getElementById("mySubmit").onclick = function(){
+    us = document.getElementById('myText').value;
+    console.log(username);
+}
