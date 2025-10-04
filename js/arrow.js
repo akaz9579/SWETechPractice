93f02ec4d8388ffc42d08763231b2135b24d124b
akaz9579 <akaz9579@gmail.com>
2025-10-03 22:21:03 -0400

    arrow,

---
+++ js/arrow.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a+b;
+}
+
+const addA = (a,b) => a+b;
+
+//same shi
+
+//when the output is more lines, use curly braces 
+
+
+const add3 = (a,b) => {
+    console.log(`add${a} and ${b}`);
+    let sum = a +b;
+    return sum;
+
+}
---
+++ js/promise.js
@@ -0,0 +1,2 @@
+const myPromise= new Promise();
+
